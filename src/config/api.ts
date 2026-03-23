3873b0844c29967876a9aacbaf6f88c133ba996e
superVT94 <trinlnse181653@fpt.edu.vn>
2026-03-23 22:35:20 +0700

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -4,7 +4,7 @@
 const isProd = import.meta.env.PROD;
 
 export const API_BASE_URL = isProd
-  ? "https://bikehub-production-1c50.up.railway.app"
+  ? "https://bikehub-production-731a.up.railway.app"
   : "";
 
 export const API_ENDPOINTS = {
